fef115919cb81dba4095223510048a93e05626a7
lucas lopes <lucascarmo6@gmail.com>
2019-11-20 20:14:06 -0200

    EulerAngles working

--- src/loco.c
+++ src/loco.c
@@ -1,13 +1,7 @@
 #include "api_robot.h"
 
-int main(int args, char **argv)
-{
-    set_head_servo(0,100);
+int main(int args, char **argv) {
+    
+    
     return 0;
-    // int a = get_us_distance();
-    // if(a > 0){
-    //     set_torque(30,30);
-    // } else {
-    //    set_torque(100,100);
-    // }
 }
