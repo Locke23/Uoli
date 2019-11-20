94aa64cd22af8fec6e74b7c6242189d9e099ebc6
lucas lopes <lucascarmo6@gmail.com>
2019-11-20 13:35:50 -0200

    torque errors

--- src/bico.s
+++ src/bico.s
@@ -1,9 +1,26 @@
 .globl set_engine_torque
 .globl set_torque
+.globl set_head_servo
+.globl get_us_distance
+.globl get_current_GPS_position
+.globl get_gyro_angles
+.globl get_time
+.globl set_time
 
-
-
+#############################################################################################
 set_torque:
+    li t1, 100
+    bgt a0, t1, erroRange # if a1 > t1 then target
+
+    li t1, -100
+    blt a0, t1, erroTorque # if a1 < t1 then target
+
+     li t1, 100
+    bgt a1, t1, erroTorque # if a1 > t1 then target
+
+    li t1, -100
+    blt a1, t1, erroTorque # if a1 < t1 then target
+    
     #passagem de parametros
     mv s0, a0
     mv s1, a1
@@ -15,10 +32,40 @@ set_torque:
     li a0, 1
     li a1, s1
     jal set_engine_torque
+    
+    erroTorque:
+        li a0, -1
+        j fim
     #retorna o contexto
-    ret
+    fim:
+        ret
+#############################################################################################
 
 set_engine_torque:
+    
+    li t1, 100
+    bgt a1, t1, erroRange # if a1 > t1 then target
+
+    li t1, -100
+    blt a1, t1, erroRange # if a1 < t1 then target
+    
     li a7, 18
     ecall
+
+    li t1, -1
+    beq a0, t1,  erroID # if a0 == t1 then target
+    
+    erroID:
+        li a0, -2
+        j fim  # jump to fim
+
+    erroRange:
+        li a0, -1
+        j fim
+
+    fim:
+        ret
+#############################################################################################
+
+set_head_servo:
     ret
